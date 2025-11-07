904c945f33b9776ccb956342962551cd77487f11
Lyall Jonathan Di Trapani <jonathan.ditrapani@assured.claims>
2025-11-07 18:42:52 -0500

    feat(aerospace): add aerospace config

--- link.py
+++ link.py
@@ -15,6 +15,7 @@ files = [
     ".config/fish/config.fish",
     ".docker/config.json",
     ".gnupg/gpg-agent.conf",
+    ".config/aerospace/aerospace.toml",
     ".config/nvim/indent/coffee.vim",
     ".config/nvim/indent/java.vim",
     ".config/nvim/indent/javascript.vim",
